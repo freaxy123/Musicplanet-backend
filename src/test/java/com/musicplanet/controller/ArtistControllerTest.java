4154e193e5e499550467556bb8317e0c3e3fded9
Freek van Lier <freek.vlier@gmail.com>
2021-06-18 13:27:55 +0200

    Added dockerfile

--- src/test/java/com/musicplanet/controller/ArtistControllerTest.java
+++ src/test/java/com/musicplanet/controller/ArtistControllerTest.java
@@ -1,65 +1,15 @@
 package com.musicplanet.controller;
 
-import com.musicplanet.entities.Artist;
-import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-//@ExtendWith(SpringExtension.class)
-@WebMvcTest(ArtistController.class)
-@ContextConfiguration(classes = {SpringBootApplication.class})
 class ArtistControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-
     @Test
-    void getAll() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/artists");
-        MvcResult result = mvc.perform(request).andReturn();
+    void getAll() {
         assertEquals(true, true);
-
-        //Arrange
-        //Act
-        //Assert
-    }
-
-    @Test
-    void get() {
-    }
-
-    @Test
-    void getByName() {
-    }
-
-    @Test
-    void add() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void deleteAll() {
     }
 }
